261c024a36a2fad66fdf5dd5e2e6a5debe57fec5
Jason <jmaalegado@gmail.com>
2025-07-07 15:17:14 +1000

    clean up custom hook files

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -13,47 +13,29 @@ export const useLogin = () => {
 		setIsPending(true);
 
 		try {
-			const res = await appAuth.signInWithEmailAndPassword(
-				email,
-				password
-			);
+			const res = await appAuth.signInWithEmailAndPassword(email, password);
 
-			/*
-			 *
-			 * Uncomment commented sections before building/deployment and remove the lines of codes after the commented ones
-			 *
-			 */
-
-			// if (!isCancelled) {
-			// 	dispatch({ type: 'LOGIN', payload: res.user });
-			// 	setError(null);
-			// 	setIsPending(false);
-			// 	// dispatch the logain action
-			// }
-			dispatch({ type: 'LOGIN', payload: res.user });
-			setError(null);
-			setIsPending(false);
-		} catch (err) {
-			// if (!isCancelled) {
-			// 	console.log(err.message);
-			// 	setError(err.message);
-			// 	setIsPending(false);
-			// }
-
-			const e = JSON.parse(err.message);
-
-			if (e.error.message === 'INVALID_LOGIN_CREDENTIALS') {
-				setError('Invalid login credentials');
-			} else {
-				setError(e.error.message);
+			if (!isCancelled) {
+				dispatch({ type: 'LOGIN', payload: res.user });
+				setError(null);
+				setIsPending(false);
 			}
-			setIsPending(false);
+		} catch (err) {
+			if (!isCancelled) {
+				const e = JSON.parse(err.message);
+				if (e.error.message === 'INVALID_LOGIN_CREDENTIALS') {
+					setError('Invalid login credentials');
+				} else {
+					setError(e.error.message);
+				}
+				setIsPending(false);
+			}
 		}
 	};
 
-	// useEffect(() => {
-	// 	return () => setIsCancelled(true);
-	// }, []);
+	useEffect(() => {
+		return () => setIsCancelled(true);
+	}, []);
 
 	return { login, error, isPending };
 };
